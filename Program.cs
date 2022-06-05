21a59fcc2f250b1f720916c65ab2fd3e5f6a1220
KessyRamos <kessyellenramos@gmail.com>
2022-06-05 09:38:25 -0300

    PrimeiroCommit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿double c = 32.5;
+double f = (9 * c + 160) / 5;
+
+Console.WriteLine("Calcular a conversão da temperatura em Fahrenheit!!!");
+
+Console.Write($"A conversão da temperatura para Fahrenheit: {f}");
